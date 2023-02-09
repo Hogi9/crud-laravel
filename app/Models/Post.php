35ea70787c5aa055bc54bb428fb385e6438eabb1
Hogi9 <hogi.pt@gmail.com>
2023-02-09 13:08:45 +0700

    Setengah video 7

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,35 +2,12 @@
 
 namespace App\Models;
 
-class Post
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title"=>"Judul Postingan Pertama",
-            "slug"=>"judul-postingan-pertama",
-            "author"=>"M Darojatun Hogi",
-            "body"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae alias, laudantium eveniet at reiciendis obcaecati quos, aspernatur blanditiis ducimus provident sit distinctio modi praesentium. Aspernatur, ea suscipit ad eaque sint id assumenda, pariatur, nostrum dicta possimus quod. Ratione totam ullam itaque modi, soluta mollitia earum labore consequuntur numquam inventore assumenda error provident saepe iusto? Amet suscipit, veniam blanditiis error neque eius iste totam facere dolores, minima modi. Fugit eaque maxime est corrupti rem, sint ut quo cumque ad error debitis.",
-        ],
-        [
-            "title"=>"Judul Postingan Kedua",
-            "slug"=>"judul-postingan-kedua",
-            "author"=>"Arvin Winata",
-            "body"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae alias, laudantium eveniet at reiciendis obcaecati quos, aspernatur blanditiis ducimus provident sit distinctio modi praesentium. Aspernatur, ea suscipit ad eaque sint id assumenda, pariatur, nostrum dicta possimus quod. Ratione totam ullam itaque modi, soluta mollitia earum labore consequuntur numquam inventore assumenda error provident saepe iusto? Amet suscipit, veniam blanditiis error neque eius iste totam facere dolores, minima modi. Fugit eaque maxime est corrupti rem, sint ut quo cumque ad error debitis.",
-        ],
-    ];
-    
-    public static function all()
-    {
-        // * bintang pada video 5
-        return collect(self::$blog_posts);
-    }
+    use HasFactory;
 
-    public static function find($slug)
-    {
-        // * bintang pada video 5
-        $posts = static::all();
-
-        //'slug' merupakan data dari variabel posts
-        return $posts->firstWhere('slug',$slug);
-    }
+    protected $guarded = ['id'];
 }
